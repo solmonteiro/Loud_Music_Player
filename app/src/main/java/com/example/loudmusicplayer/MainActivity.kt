14c43811e192dd291d682096cff7f7a88bd5ddfe
solmonteiro <sole7mares@gmail.com>
2022-07-25 23:22:12 -0300

    SeekBar volume e tempo implementadas

--- app/src/main/java/com/example/loudmusicplayer/MainActivity.kt
+++ app/src/main/java/com/example/loudmusicplayer/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.loudmusicplayer
 
+import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.widget.Button
-import android.widget.ProgressBar
 import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,13 +16,20 @@ class MainActivity : AppCompatActivity() {
     private var totalTime: Int = 0
     private lateinit var playBtn: Button
     private lateinit var volumeBar: SeekBar
+    private lateinit var positionBar: SeekBar
+    private lateinit var remainingTimeLabel:TextView
+    private lateinit var elapsedTimeLabel:TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playBtn = findViewById<Button>(R.id.playBtn)
+        playBtn = findViewById(R.id.playBtn)
+        volumeBar = findViewById(R.id.volumeBar)
+        positionBar = findViewById(R.id.positionBar)
+        remainingTimeLabel=findViewById(R.id.remainingTimeLabel)
+        elapsedTimeLabel=findViewById(R.id.elapsedTimeLabel)
 
         mp = MediaPlayer.create(this, R.raw.producao)
         mp.isLooping = true
@@ -27,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         totalTime = mp.duration
 
         //Volume Bar
-        
+
+
         volumeBar.setOnSeekBarChangeListener(
             object : SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(
@@ -51,6 +62,43 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
+        //Position Bar
+        positionBar.max = totalTime
+        positionBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    if (fromUser) {
+                        mp.seekTo(progress)
+                    }
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+            }
+        )
+
+        //Thread
+        Thread(Runnable {
+            while (mp != null) {
+                try {
+                    var msg = Message()
+                    msg.what = mp.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1000)
+                } catch (e: InterruptedException) {
+                }
+            }
+        }).start()
+
 
         playBtn.setOnClickListener {
             if (mp.isPlaying) {
@@ -64,6 +112,39 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+
+    }
+
+    @SuppressLint("HandlerLeak")
+    var handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            var currentPosition = msg.what
+
+            //Update positionBar
+            positionBar.progress = currentPosition
+
+            //Update Labels
+            var elapsedTime = createTimeLabel(currentPosition)
+            elapsedTimeLabel.text=elapsedTime
+
+            var remainingTime=createTimeLabel(totalTime-currentPosition)
+            remainingTimeLabel.text="-$remainingTime"
+        }
+
+    }
+
+
+    fun createTimeLabel(time:Int):String{
+        var timeLabel=""
+        var min =time/1000/60
+        var sec =time/1000%60
+
+        timeLabel="$min:"
+        if (sec<10)timeLabel+="0"
+        timeLabel+=sec
+
+        return timeLabel
 
     }
 
